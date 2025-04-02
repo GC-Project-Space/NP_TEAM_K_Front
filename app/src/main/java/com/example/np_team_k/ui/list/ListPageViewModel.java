f892fe20dc879921929c0a5fb68ee6aa3c2232a3
shimfff <shimseawon1510@gmail.com>
2025-04-02 21:28:12 +0900

    :sparkles: feat: 바텀네비게이션 세팅

--- app/src/main/java/com/example/np_team_k/ui/list/ListPageViewModel.java
+++ app/src/main/java/com/example/np_team_k/ui/list/ListPageViewModel.java
@@ -1,16 +1,16 @@
-package com.example.np_team_k.ui.dashboard;
+package com.example.np_team_k.ui.list;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ListPageViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ListPageViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
